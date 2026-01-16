f3da3029e4efe648cfcf37f796dae0574e9622f3
刘璐璐 <liululu44556@163.com>
2026-01-16 20:15:07 +0800

    delete auth

--- lll-auth/src/main/java/top/lll44556/lll/auth/mapper/SecurityUserMapper.java
+++
@@ -1,8 +0,0 @@
-package top.lll44556.lll.auth.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import top.lll44556.lll.auth.adapter.SecurityUser;
-
-public interface SecurityUserMapper extends BaseMapper<SecurityUser> {
-    SecurityUser selectByUsername(String username);
-}
